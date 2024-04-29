ab1b90d42c15482832100335daebd61aba73a0fb
Leksiyy <leksiy197618@gmail.com>
2024-04-29 16:09:53 +0300

    custom string class

--- 10-04-24/string.cpp
+++ 10-04-24/string.cpp
@@ -4,11 +4,8 @@ int main() {
     cout << "hello\n";
 
     String str1{};
-    cout << sizeof(str1) << endl;
     String str2{20};
-    cout << sizeof(str2) << endl;
     String str3{"Hello IsStep!!!"};
-    cout << sizeof(str3) << endl;
     str3.getStr();
     str1.cinStr("\nprogramming process 80%..");
     str1.getStr();
